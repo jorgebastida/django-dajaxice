c102d8859c87e939b4675fe62e37ce9a2d0729b2
Jorge Bastida <jorge@thecodefarm.com>
2010-06-09 23:38:09 +0200

    Added clement patch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description = "Agnostic and easy to use ajax library for django",
     download_url = "http://cloud.github.com/downloads/jorgebastida/django-dajaxice/django-dajaxice-0.1.5.tar.gz",
     url = "http://dajaxproject.com",
-    packages= ['dajaxice', 'dajaxice.templatetags', 'dajaxice.core', 'dajaxice.management.commands'],
+    packages= ['dajaxice', 'dajaxice.templatetags', 'dajaxice.core', 'dajaxice.management', 'dajaxice.management.commands'],
     package_data = {'dajaxice': ['templates/dajaxice/*']},
     classifiers=['Development Status :: 4 - Beta',
                 'Environment :: Web Environment',
