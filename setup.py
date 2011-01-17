5c38a8f1d74c4eae93789092b7a8dcc1218a7f2e
Angel Abad <angelabad@gmail.com>
2011-01-17 12:54:27 +0100

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     url = "http://dajaxproject.com",
     packages= ['dajaxice', 'dajaxice.templatetags', 'dajaxice.core', 'dajaxice.management', 'dajaxice.management.commands'],
     package_data = {'dajaxice': ['templates/dajaxice/*']},
+    long_description="""\
+Easy to use AJAX library for django, all the presentation logic
+resides outside the views and doesn't require any JS Framework.
+Dajaxice uses the unobtrusive standard-compliant (W3C) XMLHttpRequest
+1.0 object.
+""",
     classifiers=['Development Status :: 4 - Beta',
                 'Environment :: Web Environment',
                 'Framework :: Django',
