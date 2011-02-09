7da3f82a989fd594e7a57ad22a86312a6c6115af
Jorge Bastida <jorge@thecodefarm.com>
2011-02-09 09:14:45 +0100

    Updating copyright year

--- dajaxice/tests/submodules/ajax.py
+++ dajaxice/tests/submodules/ajax.py
@@ -1,5 +1,5 @@
 #----------------------------------------------------------------------
-# Copyright (c) 2009-2010 Benito Jorge Bastida
+# Copyright (c) 2009-2011 Benito Jorge Bastida
 # All rights reserved.
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
