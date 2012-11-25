264805ebbaf2c5ec77fee2ba1c9cd81b41d8f91e
Jorge Bastida <me@jorgebastida.com>
2012-11-25 23:55:11 +0100

    Fix dajaxice core JS errors

--- dajaxice/templates/dajaxice/dajaxice_module_loop.js
+++ dajaxice/templates/dajaxice/dajaxice_module_loop.js
@@ -1,9 +1,12 @@
 {{ name }}: {
     {% include "dajaxice/dajaxice_function_loop.js" %}
+    {% with parent_foorloop=forloop %}
     {% for name, sub_module in module.submodules.items %}
     {% with filename="dajaxice/dajaxice_module_loop.js" module=sub_module %}
         {% include filename %}
     {% endwith %}
-    {% if not forloop.last or module.submodules %},{% endif %}
+    {% if not forloop.last %}y,y{% endif %}
     {% endfor %}
     }
+    {% if not parent_foorloop.last %},{% endif %}
+    {% endwith %}
