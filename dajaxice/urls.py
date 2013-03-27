7d47ad55d89937007ad52e82fcc87ce62ec00539
Alex Whittemore <alexwhittemore@gmail.com>
2013-03-27 11:55:13 -0700

    Kick a travis rebuild.

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -4,6 +4,7 @@ except ImportError:
     # for Django version less then 1.4
     from django.conf.urls.defaults import url, patterns
 from .views import DajaxiceRequest
+# This commit just to kick a rebuild, since travis somehow got hung up.
 
 urlpatterns = patterns('dajaxice.views',
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
