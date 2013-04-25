2b460aec60e1f21bbddd54be6898b5392a073abd
Jan Salvador van der Ven <salvador@factlink.com>
2013-04-25 17:46:19 +0300

    Make urls.py ready for django > 1.4

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import *
+except ImportError:  # django < 1.4
+    from django.conf.urls.defaults import *
 from .views import DajaxiceRequest
 
 urlpatterns = patterns('dajaxice.views',
