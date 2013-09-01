8a8393031cf3ba761e438814c8b07014899102ab
neumond <knifeslaughter@gmail.com>
2013-09-01 23:05:28 +0400

    Python 3 issues

--- dajaxice/views.py
+++ dajaxice/views.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.conf import settings
-from django.utils import simplejson
+from django.utils import simplejson, six
 from django.views.generic.base import View
 from django.http import HttpResponse, Http404
 
@@ -9,19 +9,6 @@ from dajaxice.exceptions import FunctionNotCallableError
 from dajaxice.core import dajaxice_functions, dajaxice_config
 
 log = logging.getLogger('dajaxice')
-
-
-def safe_dict(d):
-    """
-    Recursively clone json structure with UTF-8 dictionary keys
-    http://www.gossamer-threads.com/lists/python/bugs/684379
-    """
-    if isinstance(d, dict):
-        return dict([(k.encode('utf-8'), safe_dict(v)) for k, v in d.iteritems()])
-    elif isinstance(d, list):
-        return [safe_dict(x) for x in d]
-    else:
-        return d
 
 
 class DajaxiceRequest(View):
@@ -40,10 +27,7 @@ class DajaxiceRequest(View):
 
             # Clean the argv
             if data != 'undefined':
-                try:
-                    data = safe_dict(simplejson.loads(data))
-                except Exception:
-                    data = {}
+                data = simplejson.loads(data)
             else:
                 data = {}
 
