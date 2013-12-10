ff4cf8658baf21f23345106adc72dba52d37c3fe
Jeremy Audet <ichimonji10@gmail.com>
2013-12-09 21:32:27 -0500

    Fix broken import
    Django 1.6 does not have a module named `django.conf.urls.defaults`. Remove
    references to this module and insert more appropriate imports.
    
    Also, reformat some text for the sake of readability.

--- dajaxice/tests/urls.py
+++ dajaxice/tests/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls.defaults import *
-
+from django.conf.urls import patterns, url, include
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 
 dajaxice_autodiscover()
-
 urlpatterns = patterns('',
     #Dajaxice URLS
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 from .views import DajaxiceRequest
 
 urlpatterns = patterns('dajaxice.views',
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
-    url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
+    url(r'',        DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
 )
