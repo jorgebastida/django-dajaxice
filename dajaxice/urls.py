ebce50720cbf685b8e07526188579359f558000a
isachiqba <isachiqba@gmail.com>
2017-03-29 00:58:33 -0400

    Update urls.py

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,11 +1,7 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
-
+from django.conf.urls import url
 from .views import DajaxiceRequest
 
-urlpatterns = patterns('dajaxice.views',
+urlpatterns = [
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
     url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-)
+]
