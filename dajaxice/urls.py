a633155e14ac65d2c3b6133ad8807aeef3638adf
Marco Antonio Islas Cruz <markuz@islascruz.org>
2023-06-25 15:05:21 -0600

    Use re_path instead of url

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,16 +1,18 @@
-try:
-    from django.conf.urls import *
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
+# try:
+#     from django.conf.urls import *
+# except ImportError:
+#     from django.conf.urls.defaults import patterns, url
+from django.urls import re_path
 
 from .views import DajaxiceRequest
 
-#urlpatterns = patterns('dajaxice.views',
+# urlpatterns = patterns('dajaxice.views',
 #    url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
 #    url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-#)
+# )
 
 urlpatterns = [
-    url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
-    url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-]
+    re_path(r'^(.+)/$', DajaxiceRequest.as_view(),
+            name='dajaxice-call-endpoint'),
+    re_path(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
+    ]
